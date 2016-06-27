25033f1bfab2f8f64d12ca187aec87e974e83e04
Jorrit Poelen <jhpoelen@gmail.com>
2016-06-27 10:43:12 -0700

    grin test cleanup

--- src/test/scala/org/planteome/samara/ParserGrin$Test.scala
+++ src/test/scala/org/planteome/samara/ParserGrin$Test.scala
@@ -71,39 +71,4 @@ class ParserGrin$Test extends FlatSpec with Matchers with NameFinderStatic with 
     ranks should contain(Taxon(name = "Rosaceae", rank = "family", id = 972))
   }
 
-  val apple = Crop(115)
-  val wheat = Crop(65)
-
-  "scraper" should "enable retrieval of crop ids" in {
-    val cropIds: Iterable[Crop] = cropIds
-    cropIds should contain(apple)
-    cropIds should contain(wheat)
-  }
-
-
-  "parser" should "be able to retrieve accessors for apple crops" in {
-
-    val cropIds: Seq[Crop] = Seq(apple)
-    val accessionIds: Iterable[Int] = ScraperGrin.getAccessionIds(cropIds)
-
-    println(s"counted [${accessionIds.size}] accessors")
-    println(s"counted [${accessionIds.distinct.size}] distinct accessors")
-    accessionIds.size should be > 0
-  }
-
-
-  "parser" should "be able to retrieve info for individual accessions" in {
-    val firstAccessionId = 1265054
-    val objs: Iterable[Observation] = ScraperGrin.getObservationsForAccession(firstAccessionId)
-
-    val obs: Observation = Observation("Triticum monococcum L. subsp. monococcum", List(Taxon("genus", "Triticum", 12442), Taxon("family", "Poaceae", 897), Taxon("subfamily", "Pooideae", 1472), Taxon("tribe", "Triticeae", 1317)), Descriptor(65098), Method(402008, Descriptor(65098)), "0 - RESISTANT, NO SYMPTOMS", 1265054)
-
-    objs.size should be > 0
-    objs should contain(obs)
-
-    objs.foreach(println)
-
-  }
-
-
 }
