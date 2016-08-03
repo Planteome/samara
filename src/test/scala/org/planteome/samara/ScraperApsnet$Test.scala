d624c425eb22d2c1d8b627ab58feb39c4af7e35f
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-02 19:40:42 -0700

    fix test error

--- src/test/scala/org/planteome/samara/ScraperApsnet$Test.scala
+++ src/test/scala/org/planteome/samara/ScraperApsnet$Test.scala
@@ -8,8 +8,9 @@ class ScraperApsnet$Test extends FlatSpec with Matchers  {
   "scraper" should "produce a giant list of disease interactions" in {
     val diseases = ScraperApsnet.scrapeDiseases()
 
-    diseases should contain(Disease("Bacterial canker", "Pseudomonas syringae pv. syringae van Hall 1902", "Diseases of Peach and Nectarine Peach: Prunus persica (L.) Batsch Nectarine: P. persica var. nucipersica (Suckow) C.K. Schneid."))
-    diseases should contain(Disease("Alfalfa witches’-broom", "‘Candidatus Phytoplasma asteris’", "Diseases of Alfalfa (Medicago sativa L.)"))
+    diseases should contain(Disease("Bacterial canker", "Pseudomonas syringae pv. syringae van Hall 1902", "Prunus persica"))
+    diseases should contain(Disease("Bacterial canker", "Pseudomonas syringae pv. syringae van Hall 1902", "Prunus persica var. nucipersica"))
+    diseases should contain(Disease("Alfalfa witches’-broom", "‘Candidatus Phytoplasma asteris’", "Medicago sativa L."))
 
     println(s"found ${diseases.size} disease interactions:")
 
