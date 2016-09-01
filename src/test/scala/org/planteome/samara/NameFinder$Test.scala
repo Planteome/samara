1d507b2a914fe77fa3944fe2c99a2d0c07145db4
Jorrit Poelen <jhpoelen@gmail.com>
2016-08-31 19:24:29 -0700

    some name finding code

--- src/test/scala/org/planteome/samara/NameFinder$Test.scala
+++ src/test/scala/org/planteome/samara/NameFinder$Test.scala
@@ -1,15 +1,47 @@
 package org.planteome.samara
 
+import java.io.InputStream
+import java.util.zip.GZIPInputStream
+
 import org.scalatest.{Matchers, FlatSpec}
 
+import scala.collection.immutable.HashMap
+import scala.io.Source
 
 
-class NameFinder$Test extends FlatSpec with Matchers  {
+case class TaxonMap(providedId: String, providedName: String, resolvedId: String, resolvedName: String)
+
+class NameFinder$Test extends FlatSpec with Matchers {
 
   "name finder" should "have access to taxon map" in {
-    val taxonMap = Samara.getClass.getResource("/org/eol/globi/taxon/taxonMap.tsv.gz")
-    taxonMap should not(be(null))
+    taxonMapStream should not(be(null))
   }
 
-}
+  def taxonMapStream: InputStream = {
+    new GZIPInputStream(Samara.getClass.getResourceAsStream("/org/eol/globi/taxon/taxonMap.tsv.gz"))
+  }
 
+  "name finder" should "resolve NCBI id for wheat (Triticum)" in {
+    val ncbiTaxa: Map[String, List[String]] = Source.fromInputStream(taxonMapStream)
+      .getLines()
+      .flatMap(line => {
+        val parts = line.split("\t").toList
+        if (parts.size > 3) {
+          Some(TaxonMap(parts(0), parts(1), parts(2), parts(3)))
+        } else {
+          None
+        }
+      })
+      .filter(_.resolvedId startsWith "NCBI:")
+      .foldLeft(new HashMap[String, List[String]].empty) {
+        (agg, entry) => {
+          val targetTaxonIds = agg.getOrElse(entry.providedName, List())
+          agg + (entry.providedName -> (entry.resolvedId :: targetTaxonIds).distinct)
+        }
+      }
+
+    val wheat = ncbiTaxa.keys.filter(_.startsWith("Triticum"))
+    wheat should not be empty
+    ncbiTaxa.keys.size should not be(0)
+  }
+}
