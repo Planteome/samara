a85175053974e1a69ae3298c050db951df9561a4
jhpoelen <jhpoelen@gmail.com>
2016-11-22 11:29:12 -0800

    flip linkout id columns

--- src/main/scala/org/planteome/samara/ScraperNCBILinkOut.scala
+++ src/main/scala/org/planteome/samara/ScraperNCBILinkOut.scala
@@ -5,10 +5,10 @@ object ScraperNCBILinkOut extends Scraper with ResourceUtil with NCBILinkOut {
   object Parser extends ParserGrin
 
   override def scrape() = {
-    println("from_taxon_id\tto_taxon_id")
+    println("lo_taxon_id\ttaxon_id")
     val pairs: Seq[(Int, Int)] = collectLinkOutPairsFor(findNCBITaxonIdsWithGRINTaxonLinkOut)
     pairs
-      .map(tuple => s"NCBITaxon:${tuple._2.toString}\tGRINTaxon:${tuple._1.toString}")
+      .map(tuple => s"GRINTaxon:${tuple._1.toString}\tNCBITaxon:${tuple._2.toString}")
       .foreach(println)
   }
 
